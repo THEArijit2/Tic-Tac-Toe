faede741c5bf57bb93a903d518defebd7d71c8ec
THEArijit2 <132394809+THEArijit2@users.noreply.github.com>
2025-04-20 17:00:49 +0530

    UpdateTest.java

--- app/src/androidTest/java/com/souvik/tictactoe/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/souvik/tictactoe/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.souvik.tictactoe;
+package com.orijit.tictactoe;
 
 import android.content.Context;
 
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.souvik.tictactoe", appContext.getPackageName());
+        assertEquals("com.orijit.tictactoe", appContext.getPackageName());
     }
 }
